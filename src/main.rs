4240cc57e7d1a9bbccb0ec802066c07d5e421b73
Riey <creeper844@gmail.com>
2019-11-16 14:29:37 +0900

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use byte_slice_cast::AsMutByteSlice;
+use crypto::digest::Digest;
+use crypto::sha2::Sha512;
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
+use std::io::{BufRead, BufReader};
+use structopt::StructOpt;
+
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+#[structopt(name = "gs")]
+enum Opt {
+    #[structopt(name = "gen")]
+    Gen,
+
+    #[structopt(name = "pick")]
+    Pick {
+        #[structopt(parse(from_os_str))]
+        file: PathBuf,
+    },
+}
+
+fn main() {
+    let opt = Opt::from_args();
+
+    match opt {
+        Opt::Gen => {
+            let key: String = thread_rng().sample_iter(Alphanumeric).take(10).collect();
+            println!("----key----\n{}", key);
+
+            let mut hash = Sha512::new();
+            hash.input_str(&key);
+            let hash = hash.result_str();
+
+            println!("----hash----\n{}", hash);
+        }
+        Opt::Pick { file } => {
+            let file = BufReader::with_capacity(1024 * 16, std::fs::File::open(file).unwrap());
+            let mut keys: Vec<String> = file.lines().collect::<Result<_, _>>().unwrap();
+            keys.sort();
+
+            let mut hash = Sha512::new();
+
+            for key in keys.iter() {
+                hash.input_str(key);
+            }
+
+            let mut out = [0usize; 512 / std::mem::size_of::<usize>()];
+            hash.result(out.as_mut_byte_slice());
+
+            let result = out.iter().fold(0, |acc, x| acc ^ x);
+
+            let index = result % keys.len();
+
+            println!("----picked key----");
+            println!("{}", keys[index]);
+        }
+    }
+}
